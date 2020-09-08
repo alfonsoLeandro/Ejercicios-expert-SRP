206a87b8f62278cfd3b7ee09d575f0bdbd825bf7
leandro alfonso <leamanya24.6@gmail.com>
2020-09-08 19:54:37 -0300

    Added summaries for method/classes

--- Game of life/FileReader.cs
+++ Game of life/FileReader.cs
@@ -3,8 +3,17 @@ using System.IO;
 
 namespace Game_of_life
 {
+    /// <summary>
+    /// Clase dedicada a leer el tablero desde una fuente externa, ya sea base de datos, archivos o cualquier otro medio.
+    /// </summary>
     class FileReader
     {
+        /// <summary>
+        /// Transforma un archivo de lineas de 0 y 1 en una array 2D de tipo bool.
+        /// Cumple con el principio SRP porque su unica responsabilidad es leer el archivo.
+        /// </summary>
+        /// <param name="path">El path al archivo</param>
+        /// <returns>Array 2D de tipo bool</returns>
         public static bool[,] FileToBoolArray(string path)
         {
             String[] fileLines = File.ReadAllLines(path);
